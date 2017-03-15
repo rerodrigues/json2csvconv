0a61ce44c80b65f66f6e9fa1e08c4b5137f1771f
Renato Rodrigues <renato.sp@gmail.com>
2017-03-15 01:57:21 -0300

    js formatting

--- routes/convert.js
+++ routes/convert.js
@@ -15,11 +15,24 @@ router.post('/', upload.single('json'), function (req, res, next) {
   var buffer = req.file.buffer.toString('utf8');
   var jsonContent = JSON.parse(buffer);
 
-  var fields = ['name','type','timestamp','sessionID','backendName','backendVersion','properties.contractNumber','properties.openedMap','properties.statusTechnician','properties.onTheWay','properties.travelTimeBegin','properties.travelTimeEnd'];
+  var fields = [
+    'name',
+    'type',
+    'timestamp',
+    'sessionID',
+    'backendName',
+    'backendVersion',
+    'properties.contractNumber',
+    'properties.openedMap',
+    'properties.statusTechnician',
+    'properties.onTheWay',
+    'properties.travelTimeBegin',
+    'properties.travelTimeEnd'
+  ];
+
   var csv = json2csv({ data: jsonContent.items, fields: fields });
 
-  var csvBuffer = new Buffer(csv);
-  res.end(csvBuffer);
+  res.end(new Buffer(csv));
 });
 
 module.exports = router;
